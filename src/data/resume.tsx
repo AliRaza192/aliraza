7a2856974f8ac7511e487f29c21955afb0e1353c
AliRaza <AliRaza192>
2025-04-13 16:02:16 +0500

    some changes

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -4,21 +4,23 @@ import { CodeIcon, HomeIcon, NotebookIcon, PencilLine } from "lucide-react";
 export const DATA = {
   name: "Ali Raza",
   initials: "AR",
-  url: "https://dillion.io",
+  url: "https://aliraza-eight.vercel.app/",
   description:
-    "Front-end Developer | Figma to HTML, CSS, Tailwind CSS, Shadcn ui | Wordpress Developer | React.js, Nextjs | Freelancer.",
+    "MERN Stack & Next.js Developer || MongoDb, Express.js, React.js,, Node.js || Full-Stack Web Developer",
   summary:
-    "Highly skilled Front-end Developer and Wordpress Expert with 5 years of experience in crafting exceptional digital experiences. Proficient in translating Figma designs into pixel-perfect HTML, CSS, and Tailwind CSS, with a keen eye for detail and a passion for responsive design. Adept in building dynamic user interfaces with React.js and Next.js. Proven track record of delivering high-quality solutions on time, with a strong focus on customer satisfaction. Let's collaborate to bring your vision to life!.",
-  avatarUrl: "/ali.webp",
+    "Experienced MERN Stack & Next.js Developer with a strong foundation in HTML, CSS, JavaScript, MongoDB, Express.js, React.js, and Node.js. Skilled in building full-stack web applications with clean, scalable code and modern UI/UX practices. Passionate about delivering fast, secure, and user-friendly web solutions using the latest technologies. From responsive front-ends to robust back-ends, I specialize in creating seamless digital experiences. Let's collaborate to turn your ideas into powerful, real-world applications!.",
+  avatarUrl: "/ali.jpeg",
   skills: [
+    "React Js",
+    "Next Js",
+    "Node Js",
+    "Express Js",
+    "MongoDb",
     "HTML",
     "CSS",
+    "JavaScipt",
     "Tailwind CSS",
-    "JavaScript",
-    "React JS",
-    "Next JS",
     "Shadcn ui",
-    "Wordpress",
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
@@ -59,8 +61,6 @@ export const DATA = {
     },
   },
 
-
-
   projects: [
     {
       title: "Budget Tracker",
@@ -84,7 +84,6 @@ export const DATA = {
         },
       ],
       image: "/budget-tracker1.png",
-
     },
     {
       title: "Budget Tracker",
@@ -108,7 +107,6 @@ export const DATA = {
         },
       ],
       image: "/budget-tracker1.png",
-
     },
-  ]
-}
+  ],
+};
